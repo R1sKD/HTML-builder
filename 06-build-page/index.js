06b781a190fdc6bb0706539ce24a25566005e14e
Denis Prokhorenko <denis07011997q@gmail.com>
2021-11-05 19:58:41 +0300

    feat: solve task 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 const { copyFile, mkdir, readdir } = require('fs/promises');
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 
 const assetsPath = path.join(__dirname, 'assets');
 const destinationPath = path.join(__dirname, 'project-dist');
@@ -27,6 +29,7 @@ function removeFiles(dir) {
   } catch (err) {
     console.error(err);
   }
+  emitter.emit('delete');
 }
 
 function copyDir(src, dest) {
@@ -88,8 +91,41 @@ function createStyleCss() {
   }
 }
 
-
+function createHTML() {
+  const input = fs.createReadStream(path.join(__dirname, 'template.html'), 'utf-8');
+  let output = path.join(destinationPath, 'index.html');
+  let str = '';
+  input.on('data', chunk => str += chunk);
+  input.on('end', () => {
+    const regexp = /{{\w+}}/g;
+    let nameTags = Array.from(str.matchAll(regexp));
+    for (let i = 0; i < nameTags.length; i++) {
+      let tagName = nameTags[i][0];
+      let componentPath = path.join(__dirname, 'components', `${tagName.slice(2, tagName.length - 2)}.html`);
+      fs.readFile(
+        componentPath,
+        'utf-8',
+        (err, data) => {
+          if (err) throw err;
+          str = str.replace(nameTags[i][0], data);
+          if (i === nameTags.length - 1) {
+            fs.writeFile(
+              output,
+              str,
+              (err) => {
+                if (err) throw err;
+              }
+            );
+          }
+        }
+      );
+    };
+  });
+}
 
 removeFiles(destinationPath);
-copyDir(assetsPath, path.join(destinationPath, 'assets'));
-createStyleCss();
+emitter.on('delete', () => {
+  copyDir(assetsPath, path.join(destinationPath, 'assets'));
+  createStyleCss();
+  createHTML();
+});
